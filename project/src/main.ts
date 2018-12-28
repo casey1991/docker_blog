527716fd58639e9b035ff3eae5235b4b4729144a
Casey Wang <caseywang@httpeace.com>
2018-12-28 17:41:25 +0800

    enable cors and remove redis in main config

--- project/src/main.ts
+++ project/src/main.ts
@@ -1,15 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import * as Cors from 'cors';
-import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.connectMicroservice({
-    transport: Transport.REDIS,
-    options: { url: 'redis://redis:6379' },
-  });
-  // app.enableCors();
-  await app.startAllMicroservicesAsync();
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
