66355cf3c74cc014e06982e721840debf41e686e
Casey Wang <caseywang@httpeace.com>
2019-01-04 23:11:10 +0800

    create graphql-user-context decorator for cat graphql

--- project/src/Modules/Cat/Schemas/cat.schema.ts
+++ project/src/Modules/Cat/Schemas/cat.schema.ts
@@ -1,10 +1,13 @@
 import * as mongoose from 'mongoose';
-
+const SchemaTypes = mongoose.SchemaTypes;
 export const CatSchema = new mongoose.Schema({
-  name: { type: mongoose.SchemaTypes.String, required: true },
-  age: { type: mongoose.SchemaTypes.Number, default: 0 },
-  description: { type: mongoose.SchemaTypes.String },
+  name: { type: SchemaTypes.String, required: true },
+  age: { type: SchemaTypes.Number, default: 0 },
+  description: { type: SchemaTypes.String },
   breed: {
-    type: mongoose.SchemaTypes.String,
+    type: SchemaTypes.String,
   },
+  owner: { type: SchemaTypes.ObjectId, required: true },
+  createdAt: { type: SchemaTypes.Date, default: Date.now },
+  updatedAt: { type: SchemaTypes.Date, default: Date.now },
 });
--- project/src/Modules/Cat/cat.resolver.ts
+++ project/src/Modules/Cat/cat.resolver.ts
@@ -1,9 +1,14 @@
+import { UseGuards } from '@nestjs/common';
 import { Resolver } from '@nestjs/graphql';
 // graplql actions
 import { Query, Mutation } from '@nestjs/graphql';
 import { Args } from '@nestjs/graphql';
 // services
 import { CatService } from './cat.service';
+// guards
+import { GqlAuthGuard } from '../Auth/graphql-auth.guard';
+// decorators
+import { User } from '../Auth/graphql-user-context.decorator';
 @Resolver('Cat')
 export class CatResolver {
   constructor(private readonly catService: CatService) {}
@@ -15,15 +20,18 @@ export class CatResolver {
   async cats() {
     return await this.catService.findAll();
   }
+  @UseGuards(GqlAuthGuard)
   @Mutation('createCat')
-  async createCat(@Args() cat) {
-    return await this.catService.create({ ...cat });
+  async createCat(@Args() cat, @User() user) {
+    return await this.catService.create({ ...cat, owner: user._id });
   }
+  @UseGuards(GqlAuthGuard)
   @Mutation('updateCat')
   async updateCat(@Args() cat) {
     const { id, ...rest } = cat;
     return await this.catService.update({ _id: id }, rest);
   }
+  @UseGuards(GqlAuthGuard)
   @Mutation('deleteCat')
   async deleteCat(@Args('id') id) {
     return await this.catService.delete({ _id: id });
