674d2a1991f5a95aa8977a0f5bf47b350c5f87a7
Casey Wang <caseywang@httpeace.com>
2019-01-12 21:57:54 +0800

    change port to 4444

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Transport } from '@nestjs/microservices';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.connectMicroservice({
-    transport: Transport.REDIS,
-    options: {
-      url: 'redis://redis:6379',
-    },
-  });
-  await app.startAllMicroservicesAsync();
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(4444);
 }
 bootstrap();
