18de1db6399b1064329924604412a7d14e7d7dc0
Casey Wang <caseywang@httpeace.com>
2019-01-14 15:07:09 +0800

    append readonly

--- src/Modules/User/Interfaces/user.interface.ts
+++ src/Modules/User/Interfaces/user.interface.ts
@@ -3,5 +3,5 @@ import { File } from '../../File/Interfaces/file.interface';
 export interface User extends Document {
   readonly email: string;
   readonly password: string;
-  avatars: [File];
+  readonly avatars: [File];
 }
