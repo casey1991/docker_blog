b021c317770e31074f14fc2a7b4d500e65de5480
Casey Wang <caseywang@httpeace.com>
2019-01-18 23:04:30 +0800

    mognoose add func

--- lib/Acl/mongoose-backend.ts
+++ lib/Acl/mongoose-backend.ts
@@ -1,9 +1,11 @@
 import { Connection } from 'mongoose';
 import { each as promiseEech } from 'bluebird';
+import { forEach } from 'lodash';
 import { Backend, AsyncFunc } from './interfaces';
+import { removeUnsupportedChar } from './utils';
 export class MongooseBackend implements Backend {
-  private _connection;
-  private _prefix;
+  private _connection: Connection;
+  private _prefix: string;
   constructor(connection: Connection, prefix: string) {
     this._connection = connection;
     this._prefix = prefix;
@@ -19,7 +21,12 @@ export class MongooseBackend implements Backend {
     return null;
   }
   get(bucket: string, key: string): Promise<any[]> {
-    return null;
+    // return null;
+    const collection = this._connection.collection(
+      this._prefix + removeUnsupportedChar(bucket),
+    );
+    const conditions = { key: key };
+    return collection.findOne(conditions);
   }
   unions(buckets: string[], keys: string[]): Promise<any> {
     return null;
@@ -27,7 +34,19 @@ export class MongooseBackend implements Backend {
   union(bucket: string, keys: string[]): Promise<any> {
     return null;
   }
-  add(transaction: AsyncFunc[], bucket: string, key: string, values: any[]) {}
+  add(transaction: AsyncFunc[], bucket: string, key: string, values: any[]) {
+    const conditions = { key: key };
+    const collection = this._connection.collection(
+      this._prefix + removeUnsupportedChar(bucket),
+    );
+    let columns = {};
+    forEach(values, value => {
+      columns[value] = true;
+    });
+    transaction.push(async () => {
+      collection.update(conditions, { $set: columns }, { upsert: true });
+    });
+  }
   del(transaction: AsyncFunc[], bucket: string, keys: string[]) {}
   remove(
     transaction: AsyncFunc[],
--- lib/Acl/utils.ts
+++ lib/Acl/utils.ts
@@ -1,3 +1,8 @@
 export const allowsBucket = role => {
   return 'allows_' + role;
 };
+export const removeUnsupportedChar = (text: string) => {
+  text = decodeURIComponent(text);
+  text = text.replace(/[/\s]/g, '_'); // replaces slashes and spaces
+  return text;
+};
